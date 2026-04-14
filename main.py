5c5d793ba0d0d0bc90b79854d87f047a56ed874c
ewiniarski <ewiniarski@o2.pl>
2026-04-14 10:01:05 +0200

    environment variables

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from mlops-agh-lab01!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_path = os.path.join("config", f".env.{environment}")
+    load_dotenv(dotenv_path=env_path, override=True)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
---
+++ settings.py
@@ -0,0 +1,16 @@
+# settings.py
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @field_validator("ENVIRONMENT")
+    @classmethod
+    def validate_environment(cls, value):
+        allowed_values = ("dev", "test", "prod")
+        if value not in allowed_values:
+            raise ValueError(f"Invalid environment: {value}.")
+        return value
